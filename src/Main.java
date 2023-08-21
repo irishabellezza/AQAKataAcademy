79511c6af6aa0936592db805e1ec3dfdce28c080
irishabellezza <irishabellezza@gmail.com>
2023-08-21 18:58:49 +0400

    Test done!

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Введите арифметическое выражение, для выхода введите \"end\":");
+        Scanner in = new Scanner(System.in);
+        String str = "";
+
+        while(in.hasNextLine()){
+            str = in.nextLine();
+            if(str.equals("end")) {
+                break;
+            }
+            else {
+                try {
+                    System.out.println(calc(str));
+                } catch (Exception e) {
+                    System.out.println("throws Exception");
+                }
+            }
+        }
+    }
+
+    static boolean checkString(String str) throws Exception {
+        String regex = "(\\d{1}|10)\\s*[\\+\\-\\*\\/]\\s*(\\d{1}|10)";
+
+        if(Pattern.matches(regex, str)==true)
+            return true;
+        else
+            throw new Exception();
+    }
+
+    public static String calc(String input) throws Exception{
+        int value = 0;
+
+        if(checkString(input)) {
+            int a = Integer.parseInt(input.split("\\s*[\\+\\-\\*\\/]\\s*")[0]);
+            int b = Integer.parseInt(input.split("\\s*[\\+\\-\\*\\/]\\s*")[1]);
+
+            if(input.contains("+"))
+                value = a + b;
+            else if(input.contains("-"))
+                value = a - b;
+            else if(input.contains("*"))
+                value = a * b;
+            else if(input.contains("/"))
+                value = a / b;
+        }
+        else
+            throw new Exception();
+
+        return String.valueOf(value);
+    }
+}
